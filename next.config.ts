95271e51da1599365fc6abf905da6818dc2a86bb
Aris Fathi <arisfathi04@gmail.com>
2026-01-31 22:28:24 +0800

    edited ncts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "upwxmmvgwfpwicsznljh.supabase.co",
-        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
